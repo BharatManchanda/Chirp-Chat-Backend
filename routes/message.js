b6d9e4581d618981e183e755e7a2b31a428d2b55
Bharat Manchanda <bharatmanchanda13@gmail.com>
2025-07-01 08:43:50 +0530

    update express routes

--- routes/message.js
+++ routes/message.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get('/:friendId/get-message', MessageController.getMessages);
 router.post('/:friendId/clear-chat', MessageController.clearMessage);
+router.post('/delete-messages', MessageController.deleteMessage);
 
 module.exports = router;
