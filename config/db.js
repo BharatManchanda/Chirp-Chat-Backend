01ad7a06e9cc294a12a9086c14a50d76aa719c63
Bharat Manchanda <bharatmanchanda13@gmail.com>
2026-01-25 13:42:48 +0530

    update db

--- config/db.js
+++ config/db.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 
+// Suppress strictQuery warning
+mongoose.set('strictQuery', true);
+
 const connectDB = async () => {
-	try {
-		await mongoose.connect( process.env.DATABASE_URL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log('MongoDB Connected');
-	} catch (error) {
-		console.error('Error connecting to MongoDB:', error);
-		process.exit(1);
-	}
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
